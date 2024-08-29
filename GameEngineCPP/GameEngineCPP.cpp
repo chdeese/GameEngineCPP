274c19a41ec05f077ba9a891969f757da2b746ed
Christian Deese <cdeesemail@gmail.com>
2024-08-29 11:07:52 -0500

    added raylib and test

--- GameEngineCPP/GameEngineCPP.cpp
+++ GameEngineCPP/GameEngineCPP.cpp
@@ -1,6 +1,20 @@
+#include "raylib.h"
 #include <iostream>
 
-int main()
+int main(void)
 {
-    std::cout << "Hello World!\n";
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
+
